3ddbe38b18ad8d27c70c6610899cf1149b86d712
Alpha-glit <caasi5736@gmail.com>
2023-08-14 22:21:09 +0100

    Password reset

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -1,16 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
-
-app = Flask(__name__, template_folder='templates')
-app.config['SECRET_KEY'] = "e71121f8359c7c241f56e489f91f32d7"
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e71121f8359c7c241f56e489f91f32d7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from FlaskBlog import routes
