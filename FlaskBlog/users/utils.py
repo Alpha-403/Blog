990d40e58a8539c15802350c1ee8e391b467fc60
Alpha-403 <142607766+Alpha-403@users.noreply.github.com>
2023-08-18 20:58:14 +0100

    Update utils.py

--- FlaskBlog/users/utils.py
+++ FlaskBlog/users/utils.py
@@ -4,9 +4,6 @@ from PIL import Image
 from flask import url_for, current_app
 from flask_mail import Message
 from FlaskBlog import mail
-from FlaskBlog.config import Config
-
-path = 'static/profile_pics'
 
 
 def save_picture(form_picture):
@@ -20,13 +17,13 @@ def save_picture(form_picture):
     i.thumbnail(output_size)
     i.save(picture_path)
 
-    return picture_fn, picture_path
+    return picture_fn
 
 
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender=Config.MAIL_USERNAME,
+                  sender='noreply@demo.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
