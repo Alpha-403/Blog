bc23ec6428a00b1e6176273215049be2b9955afa
Alpha-403 <142607766+Alpha-403@users.noreply.github.com>
2023-08-18 19:28:15 +0100

    Update utils.py

--- FlaskBlog/users/utils.py
+++ FlaskBlog/users/utils.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for, current_app
+from flask import url_for
 from flask_mail import Message
 from FlaskBlog import mail
 from FlaskBlog.config import Config
@@ -13,14 +13,14 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = url_for('static', filename=f'profile_pics/{picture_fn}')
 
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
 
-    return picture_fn
+    return picture_fn, picture_path
 
 
 def send_reset_email(user):
