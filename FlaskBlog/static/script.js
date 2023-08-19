fbd0dbf48830505536e5f089dcb30b08ad3e8aae
Alpha-403 <142607766+Alpha-403@users.noreply.github.com>
2023-08-19 04:16:19 +0100

    Create script.js

---
+++ FlaskBlog/static/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('change', function() {
+        if (darkModeToggle.checked) {
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+    });
+});
